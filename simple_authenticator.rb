0d3be078f5e47a469e3b3ca2eb1d150d40876532
mohsinnazakat1 <csworld7177@gmail.com>
2022-04-29 12:30:12 +0500

    initial commit

---
+++ simple_authenticator.rb
@@ -0,0 +1,12 @@
+users = [
+    {'name' =>  'mohsin', 'password' => 'abc123a'},
+    {'name' =>  'gama', 'password' =>'abc123b'},
+    {'name' =>  'nomi', 'password' =>'abc123c'},
+    {'name' =>  'ghazi', 'password' =>'abc123d'},
+    {'name' =>  'haseeb', 'password' =>'abc123e'},
+    {'name' =>  'mashoor', 'password' =>'abc123f'},
+]
+
+puts 'Welcome to Simple Authenticator'
+35.times {print '-'}
+puts
